c2ff17f3bba9b1d21acdca107e7b9be05fe7a8d3
小渣渣 <5610240+xiao_zhazha@user.noreply.gitee.com>
2020-01-19 14:53:03 +0800

    Initial commit

---
+++ menu/src/main/java/com/wxz/entity/Menu.java
@@ -0,0 +1,8 @@
+package com.wxz.entity;
+
+public class Menu {
+    private int id;
+    private String name;
+    private double price;
+    private String flavor;
+}
---
+++ order/src/main/java/com/wxz/OrderApplication.java
@@ -0,0 +1,17 @@
+package com.wxz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SpringBootApplication
+public class OrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+
+
+
+}
